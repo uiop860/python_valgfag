6496acb3cb4c87a8035cf150f860f1290316dd0f
Oliver <Oliverstaehr96@gmail.com>
2022-02-13 15:35:50 +0100

    week 2 excercises

--- week02/csv_program.py
+++ week02/csv_program.py
@@ -8,11 +8,14 @@ def print_file_content(file):
 
 def write_list_to_file(output_file, list):
   with open(output_file,'w') as output_file:
-    for id, item in enumerate(list):
-      if id == len(list):
-        output_file.write(item)
-      else:
-        output_file.write(item + '\n')
+    for id, row in enumerate(list):
+      if id != 0:
+        output_file.write('\n')
+      for id, item in enumerate(row):
+        if id != len(item):
+          output_file.write(item.strip() + ',')
+        else:
+          output_file.write(item)
   
 def read_csv(input_file):
   csv_list = []
@@ -21,18 +24,22 @@ def read_csv(input_file):
     # header_row = next(reader)
     for row in reader:
       csv_list.append(row)
-  print(csv_list)
+  return csv_list
       
     
 
 if __name__ == '__main__':
   
-  read_csv("data.csv")
+  # read_csv("data.csv")
   # write_list_to_file('test.txt',['test','abe','abe','abe','abe','abe'])
   
-  # parser = argparse.ArgumentParser(description='')
-  # parser.add_argument('file',help='The csv file to read')
+  parser = argparse.ArgumentParser(description='')
+  parser.add_argument('file',help='The csv file to read')
+  parser.add_argument('-o','--output', help='The output file to write')
+  args = parser.parse_args()
   
-  # args = parser.parse_args()
-  
-  # print_file_content(args.file)
+  if args.output:
+    csv_list = read_csv(args.file)
+    write_list_to_file(args.output, csv_list)
+  else:
+    print_file_content(args.file)
